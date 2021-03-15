785c57d625c4b6ca334d5bb07da556bc429b1c1b
SurezVijay <v.suresh2907@gmail.com>
2021-03-15 14:04:37 +0800

    1 file modified

--- Card.tsx
+++ Card.tsx
@@ -20,31 +20,14 @@ class Card extends Component<IProps, IState> {
     this.setState({ message: "Good Morning" });
   };
 
-  sayGoodAfternoon = (msg: string) => {
-    this.setState({ message: msg });
-  };
-
   render() {
     return (
       <div>
         <h3> Card Component </h3>
         <p> {this.state.message} </p>
         <button className="btn btn-primary" onClick={this.sayGoodMorning}>
-          Gud Mrg
-        </button>
-        <button
-          className="btn btn-success"
-          onClick={this.sayGoodAfternoon.bind(this, "Good Afternoon")}
-        >
-          Gud Noon
-        </button>
-        <button
-          className="btn btn-danger "
-          onClick={() => {
-            this.setState({ message: "Good Night" });
-          }}
-        >
-          Gud Nyt
+          {" "}
+          Say Morning{" "}
         </button>
       </div>
     );
